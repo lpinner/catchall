02d28005e54a6cfce822cd39db80c490e0fca79b
Luke Pinner <lpinner@users.noreply.github.com>
2023-07-25 13:12:11 +1000

    Update rescale.py

--- geospatial/rescale.py
+++ geospatial/rescale.py
@@ -4,7 +4,7 @@ License is Apache 2.0
 
 def rescale(raster, out_min=0, out_max=1):
     arr_min, arr_max = raster.minimum, raster.maximum
-    return out_min + ((((array - arr_min)) * (out_max - out_min)) / (arr_max - arr_min))
+    return out_min + ((((raster - arr_min)) * (out_max - out_min)) / (arr_max - arr_min))
 
 
 
